3d83064e511b8c66e6c00bfeb2ab7df8ac389087
lost-in-Code-au <reece.c.jones@gmail.com>
2020-02-08 14:11:04 +1100

    first start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    input: '',
+
+  }
+
+  handleInput = (e) => {
+    // console.log(e.target.value)
+    this.setState({input: e.target.value})
+  }
+
+  submitToCoder = () => {
+
+  }
+
+  render () {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <div className="App-div">
+          <h1>Welcome to Ayla's Code converter</h1>
+          <p>Simply input what you would like coded into the input below and press submit!</p>
+          <textarea onKeyDown={(e)=> this.handleInput(e)} />
+          <button onClick={this.submitToCoder} />
+          
+        </div>
+        <div className="App-div">
+          <h2>Use the table below to decode your messages</h2>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
