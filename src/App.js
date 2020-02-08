7c51d3a0c69069a3c06b45d36142627fbe86b79d
lost-in-Code-au <reece.c.jones@gmail.com>
2020-02-09 01:07:19 +1100

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import KeyboardedInput from 'react-touch-screen-keyboard';
+import 'react-touch-screen-keyboard/lib/Keyboard.css'; // if you just want css
+
+const CodeToLettersArray = [
+  'A','B'
+]
+// index = letter
+
+const SymbolsToCode = [
+  'image1','imag2 ect'
+]
+// index = symbol
+  
+
 
 class App extends React.Component {
   state = {
     input: '',
-
+    output: '===>test of state change<=='
   }
 
   handleInput = (e) => {
@@ -14,7 +28,7 @@ class App extends React.Component {
   }
 
   submitToCoder = () => {
-
+    console.log(this.state.input)
   }
 
   render () {
@@ -30,6 +44,30 @@ class App extends React.Component {
         </div>
         <div className="App-div">
           <h2>Use the table below to decode your messages</h2>
+          <KeyboardedInput
+                          enabled
+                          required
+                          // type={this.props.type}
+                          // onChange={this.props.onChange}
+                          // onBlur={this.props.onBlur}
+                          // onFocus={this.props.onFocus}
+                          // value={this.props.value}
+                          // min={this.props.min}
+                          // max={this.props.max}
+                          // step={this.props.step}
+                          // name={this.props.name}
+                          // inputClassName={this.props.inputClassName}
+                          // keyboardClassName={this.props.keyboardClassName}
+                          // placeholder={this.props.placeholder}
+                          defaultKeyboard="us"
+                          // secondaryKeyboard="us" // optional
+                          isFirstLetterUppercase={true} // optional, default is `false`
+                          uppercaseAfterSpace={true} // optional, default is `false`
+                          isDraggable={false} // optional, default is `true`
+                          // readOnly={this.props.readOnly} // optional
+                          // opacity={0.9} // optional
+          />
+          {this.state.output}
         </div>
       </div>
     )
